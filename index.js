e993c105afc9ecbd946ae3f0e8eb105bfb35dc7d
Iftakhar Husan <iftakharn19@gmail.com>
2021-08-01 06:27:52 +0600

    Init project

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+Minesweeper rules
+  - when clicked on empty cell reveal all the adjacent empty cells recursively
+  - the cell value in the middle tells how many cells around that cell has mines
+  - the cells which are empty can be shown without number in the cell
+  - when clicked on a mine reveal all the mines and show end screen
+  - when all the tiles are revealed except the mines show the wining screen
+
+
+Minesweeper ui
+  - cell colors for different type of cell
+    marked - yellow
+    boom/mine - red
+    number - green
+    hidden - theme color
+  - right mouse click to mark cell
+  - left mouse click to reveal cell
+*/
